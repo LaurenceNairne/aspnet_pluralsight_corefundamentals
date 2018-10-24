046a2d1651fb3898726ce0d4bd5035205e54eb1d
laurence-nairne <laurence.nairne@blueprintpartners.com>
2018-10-24 12:03:06 +0100

    Created custom IGreeter interface, updated startup to inject the dependency

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetMessageOfTheDay() { return _configuration["Greeting"]; }
+    }
+}
