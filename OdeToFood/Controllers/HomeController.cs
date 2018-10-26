dd35804b581a47176f638fd7843af4b43b297bce
LaurenceNairne <laurencenairne@gmail.com>
2018-10-26 14:54:05 +0100

    Updated to derive from base Controller class.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello, it's me...the HomeController.";
+            return Content("Hello, it's me...the HomeController.");
         }
     }
 }
