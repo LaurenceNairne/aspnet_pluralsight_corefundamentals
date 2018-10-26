fa8c81002739837ebca6efe69ed27cf9fac6c26e
LaurenceNairne <laurencenairne@gmail.com>
2018-10-26 14:53:17 +0100

    New controller for different response to request, derives from base Controller class.

---
+++ OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OdeToFood.Controllers
+{
+    [Route("pages/[controller]/bob/[action]")]
+    public class AboutController : Controller
+    {
+        public string Phone()
+        {
+            return "+44 07777 777 777";
+        }
+
+        public string Address()
+        {
+            return "123 Fake Street,\nMadeupville,\nNowhere";
+        }
+    }
+}
