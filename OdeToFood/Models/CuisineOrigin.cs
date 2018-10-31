36f2ddb37781f644bedf90a05e0583de20891f95
LaurenceNairne <laurencenairne@gmail.com>
2018-10-31 16:48:07 +0000

    Created an enum to hold all cuisine origins (country)

---
+++ OdeToFood/Models/CuisineOrigin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Models
+{
+    public enum CuisineOrigin
+    {
+        None,
+        American,
+        Chinese,
+        English,
+        French,
+        German,
+        Indian,
+        Italian,
+        Japanese,
+        Moroccan,
+        Spanish
+    }
+}
