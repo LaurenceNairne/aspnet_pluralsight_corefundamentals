a4237fda6b60d4ab2ffeaa642a6eda2d14f475e2
LaurenceNairne <laurencenairne@gmail.com>
2018-11-06 09:34:18 +0000

    Beginnings of my database connection.

--- OdeToFood/Data/OdeToFoodDbContext.cs
+++ OdeToFood/Data/OdeToFoodDbContext.cs
@@ -9,6 +9,12 @@ namespace OdeToFood.Data
 {
     public class OdeToFoodDbContext : DbContext
     {
+        public OdeToFoodDbContext(DbContextOptions options) 
+            : base(options)
+        {
+
+        }
+
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
